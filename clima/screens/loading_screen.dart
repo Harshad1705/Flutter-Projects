60619d97f0d4dbfa46fdfae6fa2ff1b4e28a4da8
Harshad1705 <77875603+Harshad1705@users.noreply.github.com>
2022-09-27 21:37:33 +0530

    Add files via upload

---
+++ clima/screens/loading_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async{
+    var premission =await Permission.locationWhenInUse.status;
+    // print(premission);
+    if(!premission.isGranted)
+      await Permission.location.request();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //Get the current location
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
